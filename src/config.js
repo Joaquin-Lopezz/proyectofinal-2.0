37c441f1272c79bb7ccc555e7a379f6faa2d533f
Joaquin Lopez <87286770+Joaquin-Lopezz@users.noreply.github.com>
2025-01-29 13:54:07 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 export const PORT = 8080;
 export const MONGODB_CNX_STR =
-    'mongodb+srv://UserCoder:YzyzdHbr8N94AFkK@cluster0.ixumopf.mongodb.net/proyectoFinal2?retryWrites=true&w=majority&appName=Cluster0';
+   'pass';
 export const SESSION_SECRET = 'SecretCoder';
 export const GITHUB_APP_ID = 901083;
-export const GITHUB_CLIENT_ID = 'Iv23liZ3pXxNP39n0V8m';
-export const GITHUB_CLIENT_SECRET = '33fd4ceadc992c37e5d956e3d4c63f09163c3545';
+export const GITHUB_CLIENT_ID ='pass'';
+export const GITHUB_CLIENT_SECRET ='pass';
 export const GITHUB_CALLBACK_URL = 'http://localhost:8080/githubcallback';
 
 //mail send
@@ -16,7 +16,7 @@ export const transport = nodemailer.createTransport({
     port: 587,
     auth: {
         user: 'joaquin.ariel.lopez.98@gmail.com',
-        pass: 'nmst xewa jbdt iegw',
+        pass: 'pass',
     },
 });
 
